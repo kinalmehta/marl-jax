d722cbc0a6f1a2f957efda3f5e685825e9af4ac8
Kinal Mehta <kinal.mehta11@gmail.com>
2023-09-16 11:03:36 +0530

    Update install.sh
    pin openjdk version and lab2d commit hash

--- install.sh
+++ install.sh
@@ -19,9 +19,10 @@ then
   echo "Installed prebuilt binary"
 else
   echo "Prebuilt binary incompatible, Compiling from source"
-  conda install -c conda-forge gcc=12.3 gxx=12.3 bazel=5.2 -y
+  conda install -c conda-forge gcc=12.3 gxx=12.3 bazel=5.2 openjdk=8 -y
   git clone https://github.com/deepmind/lab2d.git
   cd lab2d
+  git reset --hard 94e37d189aee8d309bc1dfae7227676c77636e88
   bazel build -c opt --define=lua=5_2 //dmlab2d:dmlab2d_wheel
   pip install bazel-bin/dmlab2d/dmlab2d-1.0-*.whl
   cd ..
